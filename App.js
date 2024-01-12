fd623705682eda2381ed2dc705bb80a9eee8a44c
Hani <126670349+hanifkrh@users.noreply.github.com>
2024-01-12 21:51:18 +0900

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import CarouselG from './components/carouselG';
+function App( ) {
+  return (
+    <div>
+      <CarouselG></CarouselG>
+    </div>
+  );
+}
+
+export default App;
